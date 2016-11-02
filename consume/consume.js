2e106a06cec05e93c08d72df726d5835141fc583
Lovinder Pnag <lspnag@gmail.com>
2016-11-01 18:09:50 -0700

    convert to functions

--- consume/consume.js
+++ consume/consume.js
@@ -21,33 +21,63 @@ $.ajax({
 var audioPaths = [];
 var audios = "https://api-dev.remeeting.com/v0.3/meeting/data/d/"
 
-$.ajax({
-  url: audios,
-  dataType: 'json',
-  headers: {
-    'Authorization': 'Bearer nGVhDpbr2wjs8FgsecMXHpdJ'
-  },
-  contentType: 'application/json',
-  success: function(result) {
-    audioPaths = result["path"]
-  },
-  error: function(error) {
-    console.log("FAIL")
-  }
-});
 
-var path = ""
 
-$(document).ajaxComplete(function() {
-  if (audioPaths.length != 0) {
-    for(var i = 0; i < audioPaths.length; ++i) {
-      if (audioPaths[i].endsWith('.m4a')){
-        path = audioPaths[i]
-      }
+function getAudioPaths() {
+  $.ajax({
+    url: audios,
+    dataType: 'json',
+    headers: {
+      'Authorization': 'Bearer nGVhDpbr2wjs8FgsecMXHpdJ'
+    },
+    contentType: 'application/json',
+    success: function(result) {
+      audioPaths = result["path"]
+      console.log(audioPaths);
+    },
+    error: function(error) {
+      console.log("FAIL")
     }
-    console.log(audioPaths);
-    console.log(path)
-    path = path.substring(1);
-    console.log(audios + path)
-  }
-})
+  });
+  conLog();
+}
+
+function conLog() {
+  console.log(audioPaths);
+}
+
+getAudioPaths();
+
+// var path = ""
+// var s3Url = ""
+
+// $(document).ajaxComplete(function() {
+//   if (audioPaths.length != 0) {
+//     for(var i = 0; i < audioPaths.length; ++i) {
+//       if (audioPaths[i].endsWith('.m4a')){
+//         path = audioPaths[i]
+//       }
+//     }
+//     console.log(audioPaths);
+//     console.log(path)
+//     path = path.substring(1);
+//     s3Url = audios + path;
+
+//     // can't do another ajax call inside of ajaxComplete
+//     $.ajax({
+//       url: s3Url,
+//       headers: {
+//         'Authorization': 'Bearer nGVhDpbr2wjs8FgsecMXHpdJ'
+//       },
+//       success: function(result) {
+//         console.log(result)
+//       },
+//       error: function(error) {
+//         console.log("FAIL")
+//       }
+//     })
+
+
+//     console.log(audios + path)
+//   }
+// })
